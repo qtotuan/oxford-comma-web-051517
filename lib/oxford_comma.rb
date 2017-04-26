065af8dea11366f27df8c78be30b7c78577e70a5
Quynh To Tuan <quynh.totuan@gmail.com>
2017-04-26 00:45:22 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,8 @@
-def oxford_comma(array)
-
+def oxford_comma(arr)
+  return arr[0] if arr.length == 1
+  return arr.join(" and ") if arr.length == 2
+  temp = arr.insert(-2, "and")
+  puts last_part = arr[-2..-1].join(" ")
+  puts first_part = arr[0..-3].join(", ")
+  first_part + ", " + last_part
 end
